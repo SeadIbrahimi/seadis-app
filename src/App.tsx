2c0780c3f3d2191746c3cef60552ffad6636a22e
Seadi <seadi@Seadis-MacBook-Air.local>
2024-05-06 23:45:26 +0200

    Added basename prop to BrowserRouter for correct routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import Navbar from "./components/Navbar";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserPosts from "./pages/UserPosts";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter  basename="/MyApp">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/user/:id/posts" element={<UserPosts />} />
       </Routes>
       <Footer/>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
