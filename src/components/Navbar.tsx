ef45b8a21b5e1d6e6dff88db1d2cd190b5d461bb
Seadi <seadi@Seadis-MacBook-Air.local>
2024-05-07 06:14:00 +0200

    update nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="bg-white border-gray-200 ">
       <div className="border-b max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="/"
+        <Link
+          to='/'
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <span className="font-extrabold self-center text-2xl whitespace-nowrap ">
             <span className="font-bold text-green-400">My</span> App
           </span>
-        </a>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-white bg-green-400 rounded md:bg-transparent md:text-green-400 md:p-0 "
-                aria-current="page"
-              >
-                Users
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-400 md:p-0 "
-              >
-                Posts
-              </a>
-            </li>
-          </ul>
-        </div>
+        </Link>
       </div>
     </nav>
   );
