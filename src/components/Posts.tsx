c6697571ccf2f898d6a0d0ab5c1be611e5d656d8
Seadi <seadi@Seadis-MacBook-Air.local>
2024-05-07 06:05:22 +0200

    update posts design

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -24,21 +24,72 @@ function Posts() {
 
   return (
     <Motions>
-      <div className="container mx-auto grid gap-5 mb-8 md:mb-12 md:grid-cols-2 ">
-        {posts &&
-          posts.map((post) => (
-            <Motions>
-              <div key={post.id} className="flex flex-col items-center rounded-lg shadow-sm justify-center p-8 text-center  md:border-e dark:bg-gray-800 dark:border-gray-700">
-                <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
-                  <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                    {post.title}
-                  </h3>
-                  <p className="my-4">{post.body}</p>
-                </blockquote>
-              </div>
-            </Motions>
-          ))}
-      </div>
+      <section
+        id="testimonials"
+        aria-label="What our customers are saying"
+        className="bg-slate-50 py-20 sm:py-32"
+      >
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl md:text-center">
+            <h2 className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
+              User Posts
+            </h2>
+          </div>
+          <ul className=" mx-auto place-items-center mt-16 grid max-w-2xl grid-cols-1 gap-6 sm:gap-8 lg:mt-20 lg:max-w-none lg:grid-cols-3">
+            {posts &&
+              posts.map((post) => (
+                <Motions>
+                  <li>
+                    <ul className="flex flex-col gap-y-6 sm:gap-y-8 ">
+                      <li>
+                        <figure className="relative rounded-2xl bg-white p-6 shadow-xl shadow-slate-900/10">
+                          <svg
+                            aria-hidden="true"
+                            width="105"
+                            height="78"
+                            className="absolute left-6 top-6 fill-slate-100"
+                          >
+                            <path d="M25.086 77.292c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622C1.054 58.534 0 53.411 0 47.686c0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C28.325 3.917 33.599 1.507 39.324 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Zm54.24 0c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622-2.11-4.52-3.164-9.643-3.164-15.368 0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C82.565 3.917 87.839 1.507 93.564 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Z"></path>
+                          </svg>
+                          <blockquote className="relative">
+                            <p className="text-lg tracking-tight text-slate-900">
+                              {post.body}
+                            </p>
+                          </blockquote>
+                          <figcaption className="relative mt-6 flex items-center justify-between border-t border-slate-100 pt-6">
+                            <div>
+                              <h2 className="font-bold italic font-display text-base text-slate-900">
+                                "{post.title}"
+                              </h2>
+                            </div>
+                            <div className="flex items-center gap-2 overflow-hidden rounded-full bg-slate-50">
+
+                            {post.reactions}
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z"
+                                />
+                              </svg>
+                            </div>
+                          </figcaption>
+                        </figure>
+                      </li>
+                    </ul>
+                  </li>
+                </Motions>
+              ))}
+          </ul>
+        </div>
+      </section>
     </Motions>
   );
 }
--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -21,120 +21,64 @@ interface User {
 }
 
 function UserDetails() {
-  const location = useLocation()
-  const selectedUser = location.pathname.split('/')[2]
+  const location = useLocation();
+  const selectedUser = location.pathname.split("/")[2];
 
   const [user, setUser] = useState<User | null>(null);
   // const [error, setError] = useState<string>("");
 
   useEffect(() => {
-
-    axios.get(`https://dummyjson.com/users/${selectedUser}`).then((res) => setUser(res.data));
+    axios
+      .get(`https://dummyjson.com/users/${selectedUser}`)
+      .then((res) => setUser(res.data));
     // .catch((err) => setError(err.message));
   }, []);
 
-
-  
   return (
     <Motions>
-    <div className="container mx-auto my-10">
-      {user && (
-        <div className="container mx-auto grid gap-5 mb-8 md:mb-12 md:grid-cols-2">
-          <div>
-            <div className="pt-6">
-              <img
-                className="h-12 w-12 flex-none rounded-full bg-gray-50"
-                src={user.image}
-                alt={user.firstName}
-              />
-            </div>
-            <div className="mt-6 border-t border-gray-100">
-              <dl className="divide-y divide-gray-100">
-                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt className="text-sm font-medium leading-6 text-gray-900">
-                    Full name
-                  </dt>
-                  <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                    {user.firstName} ({user.maidenName}) {user.lastName}
-                  </dd>
-                </div>
-                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt className="text-sm font-medium leading-6 text-gray-900">
-                    Role:
-                  </dt>
-                  <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                    Backend Developer
-                  </dd>
-                </div>
-                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt className="text-sm font-medium leading-6 text-gray-900">
-                    Email address
-                  </dt>
-                  <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                    {user.email}
-                  </dd>
-                </div>
-                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt className="text-sm font-medium leading-6 text-gray-900">
-                    Age
-                  </dt>
-                  <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                    {user.age}
-                  </dd>
-                </div>
-              </dl>
-            </div>
-          </div>
-          <div>
-            <div className="mt-6 border-t border-gray-100">
-              <div className="px-4 sm:px-0">
-                <h3 className="text-base font-semibold leading-7 text-gray-900">
-                  Information
-                </h3>
-                <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-                  Personal details:
-                </p>
+      <div className="container mx-auto my-10">
+        {user && (
+            <div className="max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
+              <div className="rounded-t-lg h-32 overflow-hidden">
+                <img
+                  className="object-cover object-top w-full"
+                  src="https://images.unsplash.com/photo-1614850715649-1d0106293bd1?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="Cover"
+                />
               </div>
-              <dl className="divide-y divide-gray-100">
-                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt className="text-sm font-medium leading-6 text-gray-900">
-                    Birth Date
-                  </dt>
-                  <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                    {user.birthDate}
-                  </dd>
-                </div>
-                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt className="text-sm font-medium leading-6 text-gray-900">
-                    Blood Group
-                  </dt>
-                  <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                    {user.bloodGroup}
-                  </dd>
-                </div>
-                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt className="text-sm font-medium leading-6 text-gray-900">
-                    Height
-                  </dt>
-                  <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                    {user.height} cm
-                  </dd>
-                </div>
-                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt className="text-sm font-medium leading-6 text-gray-900">
-                    Weight
-                  </dt>
-                  <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                    {user.weight} Kg
-                  </dd>
-                </div>
-              </dl>
+              <div className="mx-auto w-32 h-32 relative -mt-16 border-4 border-white rounded-full overflow-hidden">
+                <img
+                  className="object-cover object-center h-32"
+                  src={user.image}
+                  alt={user.firstName}
+                />
+              </div>
+              <div className="text-center mt-2">
+                <h2 className="font-semibold">
+                  {user.firstName} ({user.maidenName}) {user.lastName}
+                </h2>
+                <p className="text-gray-500">{user.phone}</p>
+              </div>
+              <div className="p-4 border-t mx-8 mt-2">
+              </div>
+              <ul className="py-4  text-gray-700 flex items-center justify-around">
+                <li className="flex flex-col items-center justify-around">
+                  <span>Weight</span>
+                  <div>{user.weight} Kg</div>
+                </li>
+                <li className="flex flex-col items-center justify-between">
+                  <span>Height</span>
+                  <div>{user.height} cm</div>
+                </li>
+                <li className="flex flex-col items-center justify-around">
+                  <span>Age</span>
+                  <div>{user.age}</div>
+                </li>
+              </ul>
             </div>
-          </div>
-        </div>
-      )}
-    </div>
-    </ Motions>
+        )}
+      </div>
+    </Motions>
   );
 }
 
